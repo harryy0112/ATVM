a5dbd928278290dc0da090f622c2d9ad23b11747
Hardik Agarwal <agarwalhardik562@gmail.com>
2023-07-30 01:20:42 +0530

    updated

--- Client/src/components/Home/home.jsx
+++ Client/src/components/Home/home.jsx
@@ -1,5 +1,7 @@
 import "./home.css";
-import train from "../../assets/TrainImg.jpg";
+import img1 from "../../assets/image-1.jpg";
+import img2 from "../../assets/image-2.jpg";
+import img3 from "../../assets/image-3.jpg";
 
 const Home = () => {
   function handleRegister() {
@@ -9,22 +11,65 @@ const Home = () => {
   return (
     <div>
       <div className="home">
-        <div className="left">
-          <img src={train} alt="train" />
+        <div className="main-slide">
+          <div
+            id="carouselExampleAutoplaying"
+            className="carousel slide"
+            data-bs-ride="carousel"
+          >
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src={img1} class="d-block w-100" alt="img1" />
+              </div>
+              <div className="carousel-item">
+                <img src={img2} class="d-block w-100" alt="img2" />
+              </div>
+              <div className="carousel-item">
+                <img src={img3} class="d-block w-100" alt="img3" />
+              </div>
+            </div>
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleAutoplaying"
+              data-bs-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleAutoplaying"
+              data-bs-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
         </div>
-        <div class="container">
-          <button className="btn" onClick={handleRegister}>
-            Using Map
-          </button>
-          <button className="btn" onClick={handleRegister}>
-            Fast Booking
-          </button>
-          <button className="btn" onClick={handleRegister}>
-            All Other Stations
-          </button>
-          <button className="btn" onClick={handleRegister}>
-            Check Train Availability
-          </button>
+        <div className="container">
+          <div class="fieldset">
+            <h1 class="legend">Book Your Ticket</h1>
+            <button className="btn" onClick={handleRegister}>
+              Using Map
+            </button>
+            <button className="btn" onClick={handleRegister}>
+              Fast Booking
+            </button>
+            <button className="btn" onClick={handleRegister}>
+              All Other Stations
+            </button>
+            <button className="btn" onClick={handleRegister}>
+              Check Train Availability
+            </button>
+          </div>
         </div>
       </div>
       <div className="down">
