f049174772aa76b4cbc6ccc18cfb0f8a1a802af8
Hardik Agarwal <agarwalhardik562@gmail.com>
2023-08-28 02:12:28 +0530

    updates

--- Client/src/pages/OtherOptions/rechargeSmartCard.jsx
+++ Client/src/pages/OtherOptions/rechargeSmartCard.jsx
@@ -3,12 +3,30 @@ import Footer from "../../components/Footer/footer";
 import "./rechargeSmartCard.css";
 
 const rechargeSmartCard = () => {
+  const back = () => {
+    alert("Transaction Cancelled");
+  };
+
+  const Submit = () => {
+    alert("Coming Soon");
+  };
   return (
     <>
       <Navbar />
-      <div className="ComingSoon">
-        <div className="atvs">Recharge Smart Card</div>
-        <div className="soon">Coming Soon</div>
+      <div className="details">
+        <div>
+          <h5>Please Enter the SmartCard Id</h5>
+        </div>
+        <div className="input-smartcard">
+          SmartCard ID : <input type="text" />
+          Enter the Amount to Add :<input type="text" />
+        </div>
+        <button className="cancel-btn-mobile" onClick={back}>
+          Cancel Transaction
+        </button>
+        <button className="Submit-btn" onClick={Submit}>
+          ADD Now
+        </button>
       </div>
       <Footer />
     </>
