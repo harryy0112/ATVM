a6e5d358342876399d7d4279613c10ccb1b6a397
Hardik Agarwal <agarwalhardik562@gmail.com>
2023-08-28 02:32:27 +0530

    updates

--- Client/src/pages/OtherOptions/rechargeSmartCard.jsx
+++ Client/src/pages/OtherOptions/rechargeSmartCard.jsx
@@ -14,19 +14,37 @@ const rechargeSmartCard = () => {
     <>
       <Navbar />
       <div className="details">
-        <div>
-          <h5>Please Enter the SmartCard Id</h5>
+        <div className="left-smartcard">
+          <div>
+            <h5>Please Enter the SmartCard Id</h5>
+          </div>
+          <div className="input-smartcard">
+            SmartCard ID : <input type="text" />
+            Enter the Amount to Add :<input type="text" />
+          </div>
+          <div className="cancel-btn-padding-mobile">
+            <button className="cancel-btn-mobile" onClick={back}>
+              Cancel Transaction
+            </button>
+            <button className="Submit-btn" onClick={Submit}>
+              ADD Now
+            </button>
+          </div>
         </div>
-        <div className="input-smartcard">
-          SmartCard ID : <input type="text" />
-          Enter the Amount to Add :<input type="text" />
+        <div className="right-smartcard">
+          <div className="help">
+            <p>
+              <b>SmartCard Details :</b>
+              <br />
+              1. SmartCard ID : XXXXXXXXXX
+              <br />
+              2. Card Balance : XXX RS.
+              <br />
+              3. Card Validity : XX-XX-20XX
+              <br />
+            </p>
+          </div>
         </div>
-        <button className="cancel-btn-mobile" onClick={back}>
-          Cancel Transaction
-        </button>
-        <button className="Submit-btn" onClick={Submit}>
-          ADD Now
-        </button>
       </div>
       <Footer />
     </>
--- Client/src/pages/OtherOptions/renewSeasonTicket.jsx
+++ Client/src/pages/OtherOptions/renewSeasonTicket.jsx
@@ -3,12 +3,49 @@ import Footer from "../../components/Footer/footer";
 import "./renewSeasonTicket.css";
 
 const renewSeasonTicket = () => {
+  const back = () => {
+    alert("Transaction Cancelled");
+  };
+
+  const Submit = () => {
+    alert("Coming Soon");
+  };
+
   return (
     <>
       <Navbar />
-      <div className="ComingSoon">
-        <div className="atvs">Renew Season Ticket</div>
-        <div className="soon">Coming Soon</div>
+      <div className="details">
+        <div className="left-smartcard">
+          <div>
+            <h5>Please Enter the SmartCard Id</h5>
+          </div>
+          <div className="input-smartcard">
+            SmartCard ID : <input type="text" />
+            Enter the Amount to Add :<input type="text" />
+          </div>
+          <div className="cancel-btn-padding-mobile">
+            <button className="cancel-btn-mobile" onClick={back}>
+              Cancel Transaction
+            </button>
+            <button className="Submit-btn" onClick={Submit}>
+              ADD Now
+            </button>
+          </div>
+        </div>
+        <div className="right-smartcard">
+          <div className="help">
+            <p>
+              <b>SmartCard Details :</b>
+              <br />
+              1. SmartCard ID : XXXXXXXXXX
+              <br />
+              2. Card Balance : XXX RS.
+              <br />
+              3. Card Validity : XX-XX-20XX
+              <br />
+            </p>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
