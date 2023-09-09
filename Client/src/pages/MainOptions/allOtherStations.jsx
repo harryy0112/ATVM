c53e99ef6c7355e16376a7f5524f408f11cd4454
Hardik Agarwal <agarwalhardik562@gmail.com>
2023-09-09 18:19:15 +0530

    updates

--- Client/src/pages/MainOptions/allOtherStations.jsx
+++ Client/src/pages/MainOptions/allOtherStations.jsx
@@ -3,12 +3,67 @@ import Footer from "../../components/Footer/footer";
 import "./allOtherStations.css";
 
 const allOtherStations = () => {
+  const back = () => {
+    alert("Transaction Cancelled");
+  };
+
+  const Submit = () => {
+    alert("Coming Soon");
+  };
+
   return (
     <>
       <Navbar />
-      <div className="ComingSoon">
-        <div className="atvs">All Other Stations</div>
-        <div className="soon">Coming Soon</div>
+      <div className="left">
+        <div className="fields">
+          <div>
+            <h5>Please Enter the Booking Id</h5>
+          </div>
+          <div className="input">
+            From : <input type="text" />
+            To : <input type="text" />
+            <label for="adult">Adults :</label>
+            <select id="adult" name="adult">
+              <option value="take">1</option>
+              <option value="take">2</option>
+              <option value="take">3</option>
+              <option value="take">4</option>
+            </select>
+            <label for="child">Child :</label>
+            <select id="child" name="child">
+              <option value="take">1</option>
+              <option value="take">2</option>
+              <option value="take">3</option>
+              <option value="take">4</option>
+            </select>
+            <label for="JourT">Journey Type :</label>
+            <select id="JourT" name="JourT">
+              <option value="take">Single</option>
+              <option value="take">Return</option>
+            </select>
+            <label for="class">Class :</label>
+            <select id="class" name="class">
+              <option value="take">II</option>
+              <option value="take">FC</option>
+            </select>
+            <label for="trainT">Train Type :</label>
+            <select id="trainT" name="trainT">
+              <option value="take">Ordinary</option>
+              <option value="take">Mail/Exp.</option>
+              <option value="take">Superfast</option>
+              <option value="take">Antodaya</option>
+              <option value="take">AC EMU</option>
+            </select>
+          </div>
+          <div className="cancel-btn-padding-mobile">
+            <button className="cancel-btn-mobile" onClick={back}>
+              Cancel Transaction
+            </button>
+            <button className="Submit-btn" onClick={Submit}>
+              Submit Now
+            </button>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
