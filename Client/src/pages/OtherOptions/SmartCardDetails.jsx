420a50d5cf4d5c1d731f7be43cc39f631cdbc564
Hardik Agarwal <agarwalhardik562@gmail.com>
2023-09-09 22:05:07 +0530

    updates

--- Client/src/pages/OtherOptions/SmartCardDetails.jsx
+++ Client/src/pages/OtherOptions/SmartCardDetails.jsx
@@ -3,12 +3,47 @@ import Footer from "../../components/Footer/footer";
 import "./SmartCardDetails.css";
 
 const sCardDetails = () => {
+  const back = () => {
+    alert("Transaction Cancelled");
+  };
+
+  const Submit = () => {
+    alert("Coming Soon");w
+  };
   return (
     <>
       <Navbar />
-      <div className="ComingSoon">
-        <div className="atvs">SmartCard Details</div>
-        <div className="soon">Coming Soon</div>
+      <div className="main">
+        <div className="left">
+          <div className="entry">
+            <div>
+              <h5>Please Enter the SmartCard Id</h5>
+            </div>
+            <div className="input">
+              SMARTCARD ID : <input type="text" />
+            </div>
+            <div className="cancel-btn-padding-mobile">
+              <button className="cancel-btn-mobile" onClick={back}>
+                Cancel Transaction
+              </button>
+              <button className="Submit-btn" onClick={Submit}>
+                Submit Now
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <div className="help">
+            <p>
+              <b>Customer Care Support :</b>
+              <br />
+              1. Customer Care Phone Number : XXXXXXXXXX
+              <br />
+              2. Customer Care Email ID : XXXXXXXXXX@gmail.com
+              <br />
+            </p>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
